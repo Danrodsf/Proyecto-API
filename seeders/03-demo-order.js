499c1728bb721a3b0f753b6139460f1c8a8911f0
Danrodsf <90701015+Danrodsf@users.noreply.github.com>
2021-11-01 10:02:28 +0100

    Mejorados seeders de orders y movies

--- seeders/03-demo-order.js
+++ seeders/03-demo-order.js
@@ -1,4 +1,5 @@
 'use strict';
+let myDate = new Date(new Date().getTime() + (5 * 24 * 60 * 60 * 1000));
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -7,102 +8,102 @@ module.exports = {
       userId: 1,
       movieId: 50,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 2,
       movieId: 1,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 3,
       movieId: 2,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 4,
       movieId: 3,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 5,
       movieId: 4,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 6,
       movieId: 5,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 7,
       movieId: 6,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 8,
       movieId: 7,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 9,
       movieId: 8,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 10,
       movieId: 9,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 11,
       movieId: 10,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 12,
       movieId: 11,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 13,
       movieId: 12,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 14,
       movieId: 13,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 15,
       movieId: 14,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 16,
       movieId: 15,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 17,
       movieId: 16,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 18,
       movieId: 17,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 19,
       movieId: 18,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     }, {
       userId: 20,
       movieId: 19,
       rentDate: new Date(),
-      returnDate: new Date
+      returnDate: myDate
     },
     ], {});
   },
